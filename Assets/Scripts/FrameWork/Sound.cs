842cadfa55c352a0ba408df3845fee78c924b130
Nguyễn Thanh Bình <binhbocnc1234@gmail.com>
2025-01-18 18:02:54 +0700

    Fix animation

--- Assets/Scripts/FrameWork/Sound.cs
+++ Assets/Scripts/FrameWork/Sound.cs
@@ -9,7 +9,7 @@ public class Sound
     public string soundName;
     public AudioClip clip;
     [Range(0f,1f)]
-    public float volume;
+    public float volume = 1f;
     [Range(0.1f, 3f)]
     public float pitch;
     public bool isLoop = false;
