df8bba885a320187e7a127a6c81082c102ab890c
Nguyễn Thanh Bình <binhbocnc1234@gmail.com>
2025-01-19 17:13:21 +0700

    Fix itemUI

--- Assets/Scripts/_GameManager/GameManager.cs
+++ Assets/Scripts/_GameManager/GameManager.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    public TextMeshProUGUI point;
+    public List<TextMeshProUGUI> itemCount = new List<TextMeshProUGUI>();
     public List<int> points;
     public static event Action<GameState> OnGameStateChanged;
 
@@ -17,7 +17,9 @@ public class GameManager : MonoBehaviour
     }
 
     private void Update() {
-        point.text = points[0].ToString();
+        for(int i = 0; i < points.Count; ++i){
+            itemCount[i].text = points[i].ToString();
+        }
     }
     public void AddScore(int scoreIndex, int val) {
         Debug.Log("Hey Hey");
